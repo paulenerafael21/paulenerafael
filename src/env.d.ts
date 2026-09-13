22ad98978b6a360e20cc8d0ad90c8061661b588a
Paulene <paulenerafael2@gmail.com>
2026-09-13 21:24:48 +0800

    Declare Ionicons gallery types

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,3 +6,9 @@ declare module '*.vue' {
 	const component: DefineComponent<Record<string, unknown>, Record<string, unknown>, unknown>;
 	export default component;
 }
+
+declare module 'ionicons/icons' {
+	export const add: string;
+	export const cloudUploadOutline: string;
+	export const trashOutline: string;
+}
